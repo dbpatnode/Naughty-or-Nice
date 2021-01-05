816ba8a06e8ff9b3e55de0cb19aca0d690aa9cd9
dbpatnodee <dbpatnode@hotmail.com>
2021-01-04 17:46:40 -0800

    refactored views out of index.jsx into App.jsx

--- src/components/layout/App.jsx
+++ src/components/layout/App.jsx
@@ -4,7 +4,74 @@ const m = require("mithril");
 import MainPage from "./MainPage.jsx";
 import NavBar from "./NavBar.jsx";
 
+// Components
+import PageBanner from "../../components/ui/PageBanner.jsx";
+import WishCardContainer from "../../components/layout/WishCardContainer.jsx";
+import WishCard from "../../components/cards/WishCard.jsx";
+
+// fake data
+import getMockData from "../../store/data";
+const SANTASLIST = getMockData();
+
+// we're returning arrays that contain these components
+const WishlistView = (santaslist) => [
+  <PageBanner
+    action={() => console.log(`logging out`)}
+    title="Santas Check List:"
+  />,
+  <WishCardContainer>
+    {santaslist.map((list) => (
+      <WishCard list={list} />
+    ))}
+  </WishCardContainer>,
+];
+
+// const ConferenceView = conferences => [
+//   <StageBanner
+//     action={() => console.log(`Logging out!`)}
+//     title="Conferences"
+//   />,
+//   <CardContainer>
+//     {conferences.map(conference => <ConferenceCard conference={conference} />)}
+//   </CardContainer>
+// ];
+
+const HomeView = () => [
+  <PageBanner
+    action={() => console.log(`Logging out!`)}
+    title="NAUGHTY OR NICE "
+  />,
+  <WishCardContainer />,
+];
+
+const FormView = () => [
+  <PageBanner
+    action={() => console.log(`Logging out!`)}
+    title="Add A child to the naughty or nice list:"
+  />,
+  <WishCardContainer />,
+];
+
 const App = {
+  // oncreate lifecycle method hook is activated after DOM ele is created.
+  // guaranteed to run @ end of render lifecycle, better fopr to get layout
+  // values or elements.
+  // lifecycle method hooks are at the same level as the view property.
+  oncreate: (vnode) => {
+    const mainPage = vnode.dom.querySelector(".main-page");
+
+    m.route(mainPage, "/home", {
+      "/home": {
+        view: () => HomeView(),
+      },
+      "/santas-list": {
+        view: () => WishlistView(SANTASLIST),
+      },
+      "/add-child": {
+        view: () => FormView(),
+      },
+    });
+  },
   view: ({ children }) => (
     <div class="App">
       <MainPage> {children}</MainPage>
--- src/index.jsx
+++ src/index.jsx
@@ -1,62 +1,52 @@
 const m = require("mithril");
 const root = document.getElementById("app");
 
-// Components
-import PageBanner from "./components/ui/PageBanner.jsx";
-import WishCardContainer from "./components/layout/WishCardContainer.jsx";
-import WishCard from "./components/cards/WishCard.jsx";
+// // Components
+// import PageBanner from "./components/ui/PageBanner.jsx";
+// import WishCardContainer from "./components/layout/WishCardContainer.jsx";
+// import WishCard from "./components/cards/WishCard.jsx";
 
 // styling
 import "./index.css";
 
-// fake data
-import getMockData from "./store/data";
-const SANTASLIST = getMockData();
+// // fake data
+// import getMockData from "./store/data";
+// const SANTASLIST = getMockData();
 
 import App from "./components/layout/App.jsx";
 
-const WishlistView = (santaslist) => (
-  <App>
-    <PageBanner
-      action={() => console.log(`logging out`)}
-      title="Santas Check List:"
-    />
-    <WishCardContainer>
-      {santaslist.map((list) => (
-        <WishCard list={list} />
-      ))}
-    </WishCardContainer>
-  </App>
-);
+// const WishlistView = (santaslist) => (
+//   <App>
+//     <PageBanner
+//       action={() => console.log(`logging out`)}
+//       title="Santas Check List:"
+//     />
+//     <WishCardContainer>
+//       {santaslist.map((list) => (
+//         <WishCard list={list} />
+//       ))}
+//     </WishCardContainer>
+//   </App>
+// );
 
-const HomeView = () => (
-  <App>
-    <PageBanner
-      action={() => console.log(`Logging out!`)}
-      title="NAUGHTY OR NICE "
-    />
-    <WishCardContainer />
-  </App>
-);
+// const HomeView = () => (
+//   <App>
+//     <PageBanner
+//       action={() => console.log(`Logging out!`)}
+//       title="NAUGHTY OR NICE "
+//     />
+//     <WishCardContainer />
+//   </App>
+// );
 
-const FormView = () => (
-  <App>
-    <PageBanner
-      action={() => console.log(`Logging out!`)}
-      title="Add A child to the naughty or nice list:"
-    />
-    <WishCardContainer />
-  </App>
-);
+// const FormView = () => (
+//   <App>
+//     <PageBanner
+//       action={() => console.log(`Logging out!`)}
+//       title="Add A child to the naughty or nice list:"
+//     />
+//     <WishCardContainer />
+//   </App>
+// );
 
-m.route(root, "/home", {
-  "/home": {
-    view: () => HomeView(),
-  },
-  "/santas-list": {
-    view: () => WishlistView(SANTASLIST),
-  },
-  "/add-child": {
-    view: () => FormView(),
-  },
-});
+m.render(root, <App />);
