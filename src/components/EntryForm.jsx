4f9836d1ea5abbd39d9a317b592ae289d867a848
dbpatnodee <dbpatnode@hotmail.com>
2021-01-05 10:59:13 -0800

    added location and naughty or nice radios to form.

--- src/components/EntryForm.jsx
+++ src/components/EntryForm.jsx
@@ -6,6 +6,17 @@ const EntryForm = {
     <form name="entry-form" id="entry-form">
       <label for="child-name">{`Childs Name`}</label>
       <input id="child-name" type="text" name="name" />
+      <label for="location">{`Location (City, State)`}</label>
+      <input id="location" type="text" name="location" />
+      <label for="naughty-or-nice">{`We're they Naughty or Nice? `}</label>
+      <label for="naughty">{`Naughty:`}</label>
+      <input type="radio" id="naughty" name="naughty" />
+      <label for="nice">{`Nice:`}</label>
+      <input type="radio" id="nice" name="nice" />
+      <UIButton
+        action={() => console.log(`Adding to list...`)}
+        buttonName="Add to list"
+      />
     </form>
   ),
 };
