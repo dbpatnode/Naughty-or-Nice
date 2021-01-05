730a9893eaed9b2eb132b2cb78e94ae706470ad6
dbpatnodee <dbpatnode@hotmail.com>
2021-01-05 11:13:25 -0800

    setting and changing state of naghty or nice.

--- src/components/EntryForm.jsx
+++ src/components/EntryForm.jsx
@@ -2,6 +2,12 @@ const m = require("mithril");
 import UIButton from "./ui/UIButton.jsx";
 
 const EntryForm = {
+  // below how you write state with Mithril (very similar to React)
+  data: {
+    naughty: false,
+    nice: false,
+  },
+
   view: (vnode) => (
     <form name="entry-form" id="entry-form">
       <label for="child-name">{`Childs Name`}</label>
@@ -10,9 +16,25 @@ const EntryForm = {
       <input id="location" type="text" name="location" />
       <label for="naughty-or-nice">{`We're they Naughty or Nice? `}</label>
       <label for="naughty">{`Naughty:`}</label>
-      <input type="radio" id="naughty" name="naughty" />
+      <input
+        type="radio"
+        id="naughty"
+        name="naughty"
+        onclick={() => {
+          vnode.state.naughty = true;
+          //   console.log(vnode.state.naughty);
+        }}
+      />
       <label for="nice">{`Nice:`}</label>
-      <input type="radio" id="nice" name="nice" />
+      <input
+        type="radio"
+        id="nice"
+        name="nice"
+        onclick={() => {
+          vnode.state.nice = true;
+          console.log(vnode.state.nice);
+        }}
+      />
       <UIButton
         action={() => console.log(`Adding to list...`)}
         buttonName="Add to list"
