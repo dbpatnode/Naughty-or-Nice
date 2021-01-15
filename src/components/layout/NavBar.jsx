dbd84c4528d517b51e182887ae88d6f4e65fc083
dbpatnodee <dbpatnode@hotmail.com>
2021-01-14 17:07:41 -0800

    added bootstrap and partially styled the navbar

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -5,16 +5,27 @@ import { home, list, smile, frown, add } from "../../services/svg.jsx"
 
 const NavBar = {
   view: () => (
-    <div class="nav-bar">
-      <span>
-        {/* paths can be named whatever you want */}
-        <NavButton path={`home`} icon={home} />
-        <NavButton path={`santas-list`} icon={list} />
-        <NavButton path={`nice-list`} icon={smile} />
-        <NavButton path={`naughty-list`} icon={frown} />
-        <NavButton path={`add-child`} icon={add} />
-      </span>
-    </div>
+    <nav class="navbar fixed-bottom navbar-expand-lg navbar-light" style="background-color: rgb(228, 114, 114)">
+      {/* <div class="nav-bar"> */}
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" style="color: white" href="#">Naughty or Nice</a>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            {/* paths can be named whatever you want */}
+
+            <li class="nav-item"><NavButton class="nav-link active" path={`home`} icon={home}>{`HOME`}</NavButton></li>
+            <li class="nav-item"><NavButton path={`santas-list`} icon={list} /></li>
+            <li class="nav-item"><NavButton path={`nice-list`} icon={smile} /></li>
+            <li class="nav-item"><NavButton path={`naughty-list`} icon={frown} /></li>
+            <li class="nav-item"><NavButton path={`add-child`} icon={add} /></li>
+          </ul>
+        </div>
+      </div>
+      {/* </div> */}
+    </nav>
   ),
 };
 
--- src/components/ui/NavButton.jsx
+++ src/components/ui/NavButton.jsx
@@ -2,7 +2,7 @@ const m = require("mithril");
 
 const NavButton = {
   view: ({ attrs }) => (
-    <a class="nav-button" href={`#!/${attrs.path}`}>
+    <a class="nav-item" href={`#!/${attrs.path}`}>
       {attrs.icon}
     </a>
   ),
