1049b7a7494afe9fd1379caecb6a549367c674ea
dbpatnodee <dbpatnode@hotmail.com>
2021-01-18 18:15:43 -0800

    deleted commented out portions and reconfigured

--- src/components/cards/NaughtyList.jsx
+++ src/components/cards/NaughtyList.jsx
@@ -12,25 +12,21 @@ const NaughtyList = {
       {/* <div class="container"> */}
       <div class="wish-card" id="parchment">
         <div class="container" id="contain">
-          <p class="inkTitle">The Naughty List</p>
           <p id="labarum"><img src={header} /></p>
-          {/* <div> */}
-          {/* <p> */}
+          <p class="inkTitle">The Naughty List</p>
+
+
           <WishField
             fieldValue={`${attrs.list.name} ${attrs.list.location}`}
             emoji={frown} />
-          {/* </p> */}
-          {/* <div style="display: flex; justify-content: flex-end"> <p class="cachet"><img id="seal" src={seal} /></p></div> */}
           <div id="signature">
             <p class="cachet"><img class="seal" src={seal} /></p>
           Santa Clause<br />123 Elf Road North Pole
           </div>
-          {/* </div> */}
         </div>
       </div>
     </div >
-    // </div >
-    // </div>
+
   ),
 };
 
