fec317548c8d3cfdb712231842699b83364fdb7e
dbpatnodee <dbpatnode@hotmail.com>
2021-01-26 17:59:45 -0800

    pushing with new styling

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,7 +1,7 @@
 const m = require("mithril");
 
 import NavButton from "../ui/NavButton.jsx";
-import { home, list, smile, frown, add } from "../../services/svg.jsx"
+import { home, list, smile, frown, add, map } from "../../services/svg.jsx"
 import img from "../../services/images/santa.png"
 
 const NavBar = {
@@ -10,22 +10,28 @@ const NavBar = {
       {/* <div class="nav-bar"> */}
       <div class="container-fluid">
         {/* button that pulls up nav menu when displayed on phone screen */}
+
+        {/* home path set to the app title in the navbar */}
+        <a class="navbar-brand" href="#">
+          Naughty or Nice
+          {/* <img src={img} alt="" width="" height="24" class="d-inline-block align-top"></img> */}
+          <img src={img} alt=""></img>
+        </a>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <a class="navbar-brand" style="color: white" href="#">
-          Naughty or Nice
-          <img src={img} alt="" width="30" height="24" class="d-inline-block align-top"></img>
-        </a>
+
+
 
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             {/* paths can be named whatever you want */}
-            <NavButton path={`home`} icon={home} linkText={` HOME`} />
             <NavButton path={`santas-list`} icon={list} linkText={` SANTAS LIST`} />
             <NavButton path={`nice-list`} icon={smile} linkText={` NICE LIST`} />
             <NavButton path={`naughty-list`} icon={frown} linkText={` NAUGHTY LIST`} />
-            <NavButton path={`add-child`} icon={add} linkText={` ADD SOMEONE TO SANTAS LIST`} />
+            <NavButton path={`add-child`} icon={add} linkText={` ADD PERSON`} />
+            <NavButton path={`map`} icon={map} linkText={` MAP`} />
           </ul>
         </div>
       </div>
