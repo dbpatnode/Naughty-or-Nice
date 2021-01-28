79cabdc691ddf0b3c580bc6658bb517fee9582e1
dbpatnodee <dbpatnode@hotmail.com>
2021-01-28 11:30:58 -0800

    started paralax and fixed responsive nave bar styling issue

--- src/components/layout/HomeContainer.jsx
+++ src/components/layout/HomeContainer.jsx
@@ -7,42 +7,107 @@ import arrow from "../../services/svg.jsx"
 const HomeContainer = {
   view: () => (
 
-
-    // <div class="snow-bg">
     <div class="container" id="home">
-      <p>Scroll down to see more</p>
-      <div class="downArrow bounce">
+      <h1><span>YOU BETTER WATCH OUT</span></h1>
+      <span class="downArrow bounce">
         <img width="100" height="100" alt="" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjwhRE9DVFlQRSBzdmcgIFBVQkxJQyAnLS8vVzNDLy9EVEQgU1ZHIDEuMS8vRU4nICAnaHR0cDovL3d3dy53My5vcmcvR3JhcGhpY3MvU1ZHLzEuMS9EVEQvc3ZnMTEuZHRkJz48c3ZnIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDMyIDMyIiBoZWlnaHQ9IjMycHgiIGlkPSLQodC70L7QuV8xIiB2ZXJzaW9uPSIxLjEiIHZpZXdCb3g9IjAgMCAzMiAzMiIgd2lkdGg9IjMycHgiIHhtbDpzcGFjZT0icHJlc2VydmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiPjxwYXRoIGQ9Ik0yNC4yODUsMTEuMjg0TDE2LDE5LjU3MWwtOC4yODUtOC4yODhjLTAuMzk1LTAuMzk1LTEuMDM0LTAuMzk1LTEuNDI5LDAgIGMtMC4zOTQsMC4zOTUtMC4zOTQsMS4wMzUsMCwxLjQzbDguOTk5LDkuMDAybDAsMGwwLDBjMC4zOTQsMC4zOTUsMS4wMzQsMC4zOTUsMS40MjgsMGw4Ljk5OS05LjAwMiAgYzAuMzk0LTAuMzk1LDAuMzk0LTEuMDM2LDAtMS40MzFDMjUuMzE5LDEwLjg4OSwyNC42NzksMTAuODg5LDI0LjI4NSwxMS4yODR6IiBmaWxsPSIjMTIxMzEzIiBpZD0iRXhwYW5kX01vcmUiLz48Zy8+PGcvPjxnLz48Zy8+PGcvPjxnLz48L3N2Zz4=" />
+      </span>
+      <div class="screen"></div>
+      <div class="flex" id="index">
+        <div class="item">
+          {/* <p>Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je anstrengender körperlicher Betätigung, außer um Vorteile daraus zu ziehen?<br /><a href="#more">More</a></p> */}
+        </div>
+        <div class="item">
+          {/* <p>Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je anstrengender körperlicher Betätigung, außer um Vorteile daraus zu ziehen?</p> */}
+        </div>
+        <div class="item">
+          {/* <p>Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je anstrengender körperlicher Betätigung, außer um Vorteile daraus zu ziehen?</p> */}
+        </div>
+        <div class="item double"></div>
+        <div class="item">
+          {/* <p>Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je anstrengender körperlicher Betätigung, außer um Vorteile daraus zu ziehen?</p> */}
+        </div>
+        <div class="item">
+          {/* <p>Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je anstrengender körperlicher Betätigung, außer um Vorteile daraus zu ziehen?</p> */}
+        </div>
+        <div class="item">
+          {/* <p>Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je anstrengender körperlicher Betätigung, außer um Vorteile daraus zu ziehen?</p> */}
+        </div>
+        <div class="item">
+          {/* <p>Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je anstrengender körperlicher Betätigung, außer um Vorteile daraus zu ziehen?</p> */}
+        </div>
+        <div class="item">
+          {/* <p>Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je anstrengender körperlicher Betätigung, außer um Vorteile daraus zu ziehen?</p> */}
+        </div>
+        <div class="item double double-2"></div>
       </div>
-
-      {/* <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src={tree} class=".imaga-fluid d-block w-100" alt="..." /> */}
-      {/* <div class="carousel-caption d-none d-md-block" style="background: white">
-              <h3 style="color: #000000">You better watch out...</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </div> */}
-      {/* </div>
-          <div class="carousel-item">
-            <img src={gift} class=".imaga-fluid d-block w-100" alt="..." /> */}
-      {/* <div class="carousel-caption d-none d-md-block">
-              <h3 style="color: #000000">You better not cry...</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </div> */}
-      {/* // </div> */}
-      {/* // <div class="carousel-item"> */}
-      {/* //   <img src={treat} class=".imaga-fluid d-block w-100" alt="..." /> */}
-      {/* <div class="carousel-caption d-none d-md-block">
-              <h3 style="color: #000000" >You better not pout, I'm telling you why...</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </div> */}
-      {/* // </div> */}
-      {/* //   </div> */}
-      {/* // </div> */}
+      <div id="more">
+        <div class="screen second-screen">
+          {/* <p>Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je anstrengender körperlicher Betätigung, außer um Vorteile daraus zu ziehen?</p>
+          <p>Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je anstrengender körperlicher Betätigung, außer um Vorteile daraus zu ziehen?<br />
+            <a href="#index">Back to Articles</a>
+          </p> */}
+        </div>
+      </div>
     </div>
-    // </div>
+    // </div >
   )
 };
 
 export default HomeContainer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={tree} class=".imaga-fluid d-block w-100" alt="..." /> */}
+{/* <div class="carousel-caption d-none d-md-block" style="background: white">
+              <h3 style="color: #000000">You better watch out...</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div> */}
+{/* </div>
+          <div class="carousel-item">
+            <img src={gift} class=".imaga-fluid d-block w-100" alt="..." /> */}
+{/* <div class="carousel-caption d-none d-md-block">
+              <h3 style="color: #000000">You better not cry...</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div> */}
+{/* // </div> */ }
+{/* // <div class="carousel-item"> */ }
+{/* //   <img src={treat} class=".imaga-fluid d-block w-100" alt="..." /> */ }
+{/* <div class="carousel-caption d-none d-md-block">
+              <h3 style="color: #000000" >You better not pout, I'm telling you why...</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div> */}
+{/* // </div> */ }
+{/* //   </div> */ }
+{/* // </div> */ }
