94aa8490c660c601d97cc3b66977a8a9096ac916
dbpatnodee <dbpatnode@hotmail.com>
2021-02-02 12:40:50 -0800

    more styling

--- src/components/EntryForm.jsx
+++ src/components/EntryForm.jsx
@@ -47,24 +47,20 @@ const EntryForm = {
 
   view: (vnode) => (
 
-    // <div>
-    //   <div class="container" id="contain">
-
-    //   </div>
-
     <form name="entry-form" class="parchment" id="entry-form">
       <p id="labarum"><img src={header} /></p>
-      <p class="inkTitle">The Naughty List</p>
-      <label for="child-name">{`Childs Name`}</label>
+      <p class="inkTitle">Add Someone To the List</p>
+      <label for="child-name">{`Childs Name:  `}</label>
       <input id="child-name" type="text" name="name" />
-      <label for="location">{`Location (City, State)`}</label>
+      <label for="location">{`Location (City, State):`}</label>
       <input
         id="location"
         type="text"
         name="location"
       />
-      <label for="naughty-or-nice">{`We're they Naughty or Nice? `}</label>
 
+      <br />
+      <label for="naughty-or-nice">{`We're they Naughty or Nice? `}</label>
 
       {/* TERNARY FOR CHANGING CLASS NAME (STYLING) */}
       {vnode.state.naughty == true ?
